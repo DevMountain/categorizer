34754ef070cbdb46b58d181f974f1e4e99e9a073
Ryan Walsh <ryan@thehatrack.net>
2017-02-28 09:10:45 -0700

    comment explaining ActiveChart, delete cra readme

--- src/components/ActiveChart/ActiveChart.js
+++ src/components/ActiveChart/ActiveChart.js
@@ -1,3 +1,11 @@
+/*
+This component is already completed, you should not need to make any changes here.
+It expects a "chart" object as a prop. The chart object should have two properties
+1: Labels: An array of strings.
+2: Datasets: An array of numbers
+*/
+
+
 import React, { Component, PropTypes } from "react";
 import Chart from "chart.js";
 
@@ -18,10 +26,10 @@ export default class ActiveChart extends Component {
 
 	componentDidMount() {
 		this.chart = new Chart( this.canvas, {
-			type: 'radar',
-			data: {
-				labels: this.props.chart.labels,
-				datasets: this.props.chart.datasets.map( this.addColorToDatasets )
+			  type: 'radar'
+			, data: {
+				  labels: this.props.chart.labels
+				, datasets: this.props.chart.datasets.map( this.addColorToDatasets )
 			}
 			, options: {
 				maintainAspectRatio: false
