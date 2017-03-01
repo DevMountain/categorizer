663deec9e3f54525a0a772650d8c647cf8e36831
Ryan Walsh <ryan@thehatrack.net>
2017-03-01 10:24:30 -0700

    formatting

--- src/ducks/chart.js
+++ src/ducks/chart.js
@@ -1,7 +1,7 @@
 const CREATE_CHART = "CREATE_CHART";
 
 const initialState = {
-	activeChartIndex: 0
+	  activeChartIndex: 0
 	, charts: [ {
 		// Labels corresponding to the corners of the chart.
 		  labels: [ "Red", "Blue", "Yellow", "Green", "Purple", "Orange" ]
@@ -11,13 +11,13 @@ const initialState = {
 		, datasets: [
 			{
 				// The name of the dataset
-				label: "My First dataset"
+				  label: "My First dataset"
 				// Each of these numbers corresponds to one of the labels above,
 				// based on index
 				, data: [65, 59, 90, 81, 56, 55, 40]
 			}
 			, {
-				label: "My Second dataset"
+				  label: "My Second dataset"
 				, data: [28, 48, 40, 19, 96, 27, 100]
 			}
 		]
@@ -34,8 +34,6 @@ export default function chart( state = initialState, action ) {
 		default: return state;
 	}
 }
-
-console.log( JSON.stringify( chart( undefined, createChart( [ "foo", "bar", "baz" ], "test" ) ), null, 2 ) );
 
 export function createChart( labels, name ) {
 	return {
