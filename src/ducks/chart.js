e19eeb35ba3fdd53a35bb19591042c66b12060be
Ryan Walsh <ryan@thehatrack.net>
2017-03-02 11:45:00 -0700

    remove comments

--- src/ducks/chart.js
+++ src/ducks/chart.js
@@ -4,17 +4,11 @@ const SET_ACTIVE_CHART_INDEX = "SET_ACTIVE_CHART_INDEX";
 const initialState = {
 	  activeChartIndex: 0
 	, charts: [ {
-		// Labels corresponding to the corners of the chart.
 		  labels: [ "Red", "Blue", "Yellow", "Green", "Purple", "Orange" ]
-		// The name of the chart
 		, name: "Example Chart"
-		// The data required for rendering values to the chart
 		, datasets: [
 			{
-				// The name of the dataset
 				  label: "My First dataset"
-				// Each of these numbers corresponds to one of the labels above,
-				// based on index
 				, data: [65, 59, 90, 81, 56, 55, 40]
 			}
 			, {
